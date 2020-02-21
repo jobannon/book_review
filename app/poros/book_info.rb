c29d0a4ccb3ccd772c2147e261ae0d94cc63d984
Joshua O'bannon <jtobannon@gmail.com>
2020-02-21 11:27:02 -0700

    Need to add genres and additional tests for publication date

--- app/poros/book_info.rb
+++ app/poros/book_info.rb
@@ -1,8 +1,12 @@
 class BookInfo
-  attr_reader :author, :title
+  attr_reader :author, :title, :genres
   def initialize(info)
     @author = info[:docs].first[:author_name]
     @title = info[:docs].first[:title_suggest]
+    @genres = info[:docs].map[:subjects] do |subject|
+      subject
+    end
+    binding.pry 
   end
 
   def get_book
--- app/poros/summary_info.rb
+++ app/poros/summary_info.rb
@@ -1,13 +1,16 @@
 class SummaryInfo 
   attr_reader :summarys_array
   def initialize(info)
-    @summarys_array = info[:results].map do |book|
-      book[:summary]
+    #not ideal to put here
+    @summarys_array = info[:results].reduce(Array.new) do |acc, book|
+      acc  << book[:summary]
+      acc << book[:publication_dt]
+      acc
     end
   end
 
-  def get_book
-    
-
+  def summarys_array_count
+    @summarys_array.count - 2 # not ideal
   end
+
 end
